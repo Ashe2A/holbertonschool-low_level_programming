1b94768292ba4b1d08d1f79f26957f0dd0a436df
Ashe2A <supersonicx001@gmail.com>
2024-02-20 09:35:49 +0000

    HW-5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a peculiar String
+ *
+ * Return: 0 (void)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,")
+return (0);
+}
