9d5cf427e70319f9aa9aad22fa3753fa8bb15056
Ashe2A <supersonicx001@gmail.com>
2024-02-21 14:14:08 +0000

    Var-2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Lists all lowercase letters
+ * abcd - Each lowercase letters
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char abcd;
+for (abcd = 'a' ; abcd <= '' ; abcd++) {
+putchar(abcd);
+}
+return (0);
+}
