743884f1ed4288731e5140c691c32b01e4778bc8
Ashe2A <supersonicx001@gmail.com>
2024-02-22 12:27:41 +0000

    Var-9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Lists 0-9
+ * zn - Each number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char zn;
+for (zn = 0 ; zn <= 9 ; zn++)
+{
+putchar(zn+48);
+putchar(32);
+putchar(44);
+}
+putchar(10);
+return (0);
+}
