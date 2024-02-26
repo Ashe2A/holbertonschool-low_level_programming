a771a95b8cab1391b67cb6a690f7b7d09d6e2288
Ashe2A <supersonicx001@gmail.com>
2024-02-26 20:22:12 +0000

    More-5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,18 @@ int i, j, n;
 
 for (n = 0 ; n < 10 ; n++)
 {
-for (i = '0' ; i <= '9' ; i++)
+for (i = '0' ; i <= '1' ; i++)
 {
-for (j = '0' ; j <= '4' ; j++)
+for (j = '0' ; j <= '9' ; j++)
+{
+if (i == '1' && j > '4')
 {
 if (i != '0')
 {
 _putchar(i);
 }
 _putchar(j);
+}
 }
 }
 _putchar('\n');
