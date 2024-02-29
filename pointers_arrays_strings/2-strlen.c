2781104657375b3696789e07238efca95b6729dc
Ashe2A <supersonicx001@gmail.com>
2024-02-29 13:28:35 +0000

    Point-2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strlen - length of character string
+ * @s: the string
+ *
+ * Return: length of string, 0 if empty
+ */
+
+int _strlen(char *s)
+{
+	int len = 1;
+
+	if (*s[0] >= 0 && *s[0] <= 255)
+	{
+		for (i = 0 ; i < len ; i++)
+		{
+			if (*s[i] >= 0 && *s[i] <= 255)
+			{
+				len++;
+			}
+		}
+	}
+	else
+	{
+		len = 0;
+	}
+	return (len);
+}
