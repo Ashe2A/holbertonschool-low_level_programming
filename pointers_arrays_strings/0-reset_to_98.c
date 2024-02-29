30d52b154a33cbfe6d0d0677923352320736ecc6
Ashe2A <supersonicx001@gmail.com>
2024-02-29 13:07:40 +0000

    Point-0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
-/* reset_to_98 - resets integer parameter to 98
- * @n - the integer in question
+/**
+ *reset_to_98 - resets integer parameter to 98
+ * @n: the integer in question
  */
 
 void reset_to_98(int *n)
