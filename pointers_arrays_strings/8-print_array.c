e52a1cc833f77b6ef0aa99b23be37de1d11a862b
Ashe2A <supersonicx001@gmail.com>
2024-03-01 02:29:35 +0000

    Point-8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - multiple integers
+ * @a: list of integers
+ * @n: number of integers
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf(a[i]);
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
