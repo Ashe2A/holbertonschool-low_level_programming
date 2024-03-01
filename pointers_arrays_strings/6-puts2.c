b3d2d42da7629eb9a1a1a7f644ffff799ac48568
Ashe2A <supersonicx001@gmail.com>
2024-03-01 02:03:24 +0000

    Point-6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - half of a string
+ * @str: the string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; i < _strlen(s) ; i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
