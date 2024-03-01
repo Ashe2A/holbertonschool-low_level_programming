a233084e626f1e356ae510d5a90a4551a27fe937
Ashe2A <supersonicx001@gmail.com>
2024-03-01 01:00:41 +0000

    Point-4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "2-strle.c"
+
+/**
+ * print_rev - reverse a string
+ * @s: the string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(s) ; i > 0 ; i--)
+	{
+		_putchar(str[i - 1]);
+	}
+	_putchar('\n');
+}
