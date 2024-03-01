f53a07febd3bc44ea8433f85755bcd451d685d5b
Ashe2A <supersonicx001@gmail.com>
2024-03-01 02:10:58 +0000

    Point-6

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - latter half of a string
+ * @str: the string
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = (_strlen(str) - 1) / 2 ; i < _strlen(str) ; i ++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
