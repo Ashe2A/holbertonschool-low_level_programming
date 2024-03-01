4dccc8adb7279b6b67d8dd1db16ca26a277944bf
Ashe2A <supersonicx001@gmail.com>
2024-03-01 11:09:54 +0000

    Point-9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strcpy - copy string
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; i <= _strlen(src) ; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+
+	char *ret;
+	ret = &dest;
+
+	return (ret);
+}
