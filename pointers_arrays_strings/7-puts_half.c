aee109f1eae3dcbc4521099fcb6d1cdbf9f935ab
Ashe2A <supersonicx001@gmail.com>
2024-03-01 02:16:16 +0000

    Point-7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ void puts_half(char *str)
 
 	for (i = _strlen(str) / 2 ; i < _strlen(str) ; i ++)
 	{
-		if (_strlen(str) % 2 == 0 && i == _strlen(str) / 2)
+		if (!(_strlen(str) % 2 == 0 && i == _strlen(str) / 2))
 		{
 			_putchar(str[i]);
 		}
