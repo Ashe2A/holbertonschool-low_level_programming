83f316f8a68eec628a78b99d3d80bfd012991269
Ashe2A <supersonicx001@gmail.com>
2024-03-01 15:48:53 +0000

    Point-10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _atoi - first integer in a string
+ * @s: string
+ *
+ * Return: the integer
+ */
+
+int _atoi(char *s)
+{
+	int i, start, final;
+	char *integer;
+
+	for (i = 0 ; i <= _strlen(s) ; i++)
+	{
+		while (*(s + i) >= '0' && *(s + i) <= '9')
+		{
+				
+		}
+	}
+
+	for (i = 0 ; i < _strlen(integer) ; i++) 
+	{
+		
+	}
+
+	return (final);
+}
