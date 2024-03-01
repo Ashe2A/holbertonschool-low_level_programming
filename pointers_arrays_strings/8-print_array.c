6502e0668e5d13f1b52c390b0acdd52caecc22a8
Ashe2A <supersonicx001@gmail.com>
2024-03-01 09:01:39 +0000

    Point-8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,15 +11,18 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0 ; i < n ; i++)
+	if (*a != '\0' && n > 0)
 	{
-		if (i != n - 1)
+		for (i = 0 ; i < n ; i++)
 		{
-			printf("%d, ", *(a + i));
-		}
-		else
-		{
-			printf("%d\n", *(a + i));
+			if (i != n - 1)
+			{
+				printf("%d, ", *(a + i));
+			}
+			else
+			{
+				printf("%d\n", *(a + i));
+			}
 		}
 	}
 }
