35f75cd333e97bc1023c6d2a819eb174473e15d9
Ashe2A <supersonicx001@gmail.com>
2024-06-18 08:54:31 +0000

    5. NANBAZ

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print base 10 digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
