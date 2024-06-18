bf900dc29607ec5363af4034c8330e9cae90f7b6
Ashe2A <supersonicx001@gmail.com>
2024-06-18 09:03:10 +0000

    6. numberz ascii

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print base 10 digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 30 ; i <= 39 ; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
