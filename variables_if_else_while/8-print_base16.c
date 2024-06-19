7d6988d397386617ed9d4d8fbd7f6be74270e507
Ashe2A <supersonicx001@gmail.com>
2024-06-19 09:58:15 +0000

    8. hexadigits

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print base 16 digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 'a' ; i <= 'f' ; i++)
+	{
+	        putchar(i);
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
