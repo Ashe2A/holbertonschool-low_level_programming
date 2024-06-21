1843723251f1ef562bfc03f660816e0782325484
Ashe2A <supersonicx001@gmail.com>
2024-06-21 12:17:48 +0000

    1. print alphabet

---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define FUNCTIONS
 
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif
