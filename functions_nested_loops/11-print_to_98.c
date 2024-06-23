0e50f379e04c3268ae58c51452e0cbe72e34b645
Ashe2A <supersonicx001@gmail.com>
2024-06-23 22:00:53 +0000

    11. trial B

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n <= 98)
+	if (n < 98)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
@@ -21,7 +21,7 @@ void print_to_98(int n)
 			}
 		}
 	}
-	else
+	else if (n > 98)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
@@ -33,5 +33,10 @@ void print_to_98(int n)
 			}
 		}
 	}
+	else
+	{
+		printf("%d\n", n);
+	}
+
 	_putchar('\n');
 }
