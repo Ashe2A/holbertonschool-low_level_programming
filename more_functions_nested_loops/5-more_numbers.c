ba6b8e0e2d50cf7c733424dd855374f4289e2d3d
Ashe2A <supersonicx001@gmail.com>
2024-06-23 22:34:57 +0000

    5. 0 to 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints all digits
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = '0' ; i <= '1' ; i++)
+	{
+		for (j = '0' ; j <= '9' ; j++)
+		{
+			if (i < '1')
+			{
+				_putchar(j);
+			}
+			else if (i >= '1' && j <= '4')
+			{
+				_putchar(i);
+				_putchar(j);
+			}
+			else
+			{
+			}
+		}
+	}
+
+	_putchar('\n');
+}
