0d9506549b125328c0ed07cc0c178d1d0784156a
Ashe2A <supersonicx001@gmail.com>
2024-06-23 22:53:51 +0000

    6. last line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - line
+ * @n: length of line
+ *
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n');
+}
