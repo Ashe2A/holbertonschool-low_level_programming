45ebf93ed1566aaaad94a2a274ac4f7fcf7e27c5
Ashe2A <supersonicx001@gmail.com>
2024-06-24 08:48:29 +0000

    10. triangle trial C

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,22 +9,24 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	for (i = 0 ; i <= size ; i++)
-	{
-		for (j = 1 ; j < size - i ; j++)
+	if (size > 0)
+	{	
+		for (i = 0 ; i <= size ; i++)
 		{
-			_putchar(' ');
-		}
+			for (j = 1 ; j <= size - i ; j++)
+			{
+				_putchar(' ');
+			}
 
-		for (k = 0 ; k < i ; k++)
-		{
-			_putchar('#');
-		}
+			for (k = 0 ; k < i ; k++)
+			{
+				_putchar('#');
+			}
 
-		_putchar('\n');
+			_putchar('\n');
+		}
 	}
-
-	if (size <= 0)
+	else
 	{
 		_putchar('\n');
 	}
