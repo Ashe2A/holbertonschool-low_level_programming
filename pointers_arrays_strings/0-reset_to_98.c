474941a342815841f5f86a4979442bb083bedc15
Ashe2A <supersonicx001@gmail.com>
2024-06-24 09:10:06 +0000

    0 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - turns pointed int to 98
+ * @n: pointer of int
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION
+#define FUNCTION
+
+void reset_to_98(int *n);
+
+#endif
