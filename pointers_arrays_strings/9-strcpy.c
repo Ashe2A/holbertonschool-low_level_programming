51fe68173319a963b10ca9c7e5946b97d97aa9cc
Ashe2A <supersonicx001@gmail.com>
2024-06-26 13:24:24 +0000

    9. copy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "2-strlen.c"
+#include "main.h"
+
+/**
+ * _strcpy - copy string
+ * @dest: destination (paste)
+ * @src: source (copy)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; i <= _strlen(src) ; i++)
+	{
+		*(dest + i) = src[i];
+	}
+
+	return (dest);
+}
