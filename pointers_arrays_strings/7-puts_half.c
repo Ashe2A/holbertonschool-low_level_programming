6e2c62dc6f3db82cfe38e23165f713d5e708659b
Ashe2A <supersonicx001@gmail.com>
2024-06-26 09:08:16 +0000

    7. put half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,18 +8,8 @@
 void puts_half(char *str)
 {
 	int i;
-	int half_size;
 
-	if (_strlen(str) % 2 == 0)
-	{
-		half_size = _strlen(str) / 2;
-	}
-	else
-	{
-		half_size = (_strlen(str) - 1) / 2;
-	}
-
-	for (i = half_size ; i < _strlen(str) ; i++)
+	for (i = _strlen(str) / 2 ; i < _strlen(str) ; i++)
 	{
 		_putchar(str[i]);
 	}
