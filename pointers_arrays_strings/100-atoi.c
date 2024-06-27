0f5b6bbd17245a080e39fde8748d4798aaab8bbd
Ashe2A <supersonicx001@gmail.com>
2024-06-27 13:24:42 +0000

    10. cond corr

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,14 +12,16 @@ int _atoi(char *s)
 	int i = 0;
 	int sign = 1;
 	unsigned int res = 0;
+	int intfound = 0;
 
 	for (i = 0 ; i < _strlen(s) ; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			res = (res * 10) + (s[i] - '0');
+			intfound = 1;
 		}
-		else if (s[i] == '-' && res == 0)
+		else if (s[i] == '-' && intfound == 0)
 		{
 			sign *= -1;
 		}
