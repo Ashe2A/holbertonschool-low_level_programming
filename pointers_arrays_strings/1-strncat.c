abd70ffc2e08ca8e02c73452bcced23b73f10efb
Ashe2A <supersonicx001@gmail.com>
2024-06-28 14:00:23 +0000

    1. n bytes

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "2-strlen.c"
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strncat - concatene strings
+ * @dest: destination
+ * @src: source
+ * @n: number of chars from src
+ *
+ * Return: concatened src with dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	char *cat = dest;
+
+	dest += _strlen(dest);
+
+	for (i = 0 ; i < n ; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+
+	*(dest + i) = '\0';
+
+	return (cat);
+}
