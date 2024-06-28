33a61a8f83e016c49753492e05d9435556f92320
Ashe2A <supersonicx001@gmail.com>
2024-06-28 12:02:38 +0000

    0. concat string

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "2-strlen.c"
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strcat - concatene strings
+ * @dest: destination
+ * @src: source
+ *
+ * Return: concatened src with dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	char *cat = (char *)malloc((_strlen(dest) + _strlen(src) + 1) * sizeof(char));
+
+	for (i = 0 ; i < _strlen(dest) ; i++)
+	{
+		cat[i] = dest[i];
+	}
+
+	for (i = 0 ; i <= _strlen(src) ; i++)
+	{
+		cat[i + _strlen(dest)] = src[i];
+	}
+
+	return (cat);
+}
