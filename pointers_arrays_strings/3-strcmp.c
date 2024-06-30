33aff9bfc90b910bf182c27574b19a8a017bc6a3
Ashe2A <supersonicx001@gmail.com>
2024-06-30 20:22:44 +0000

    3. compare strings trial A

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "2-strlen.c"
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strcmp - compare strings
+ * @s1: first str
+ * @s2: second str
+ *
+ * Return: first diff char diff
+ */
+char *_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0 ; s1[i] == s2[i] ; i++)
+	{
+	}
+
+	return (s1[i] - s2[i]);
+}
