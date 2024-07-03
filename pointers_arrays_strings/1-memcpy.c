9771c803a047664a54ae742d95ddc683a430f1c1
Ashe2A <supersonicx001@gmail.com>
2024-07-04 01:15:11 +0200

    back to n simple

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -13,7 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
     unsigned int i;
 
-    for (i = 0; (i < n) && (i < _strlen(src) || i < _strlen(dest)) ; i++)
+    for (i = 0; i < n ; i++)
     {
         *(dest + i) = *(src + i);
     }
