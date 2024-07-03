469537be53f0e96d24346eb16cd613b5f3cfeb85
Ashe2A <supersonicx001@gmail.com>
2024-07-04 01:08:33 +0200

    1 trial A

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _memcpy - copy n first elements
+ * @dest: string to overwrite
+ * @src: string to copy
+ * @n: number of chars to fill
+ *
+ * Return: overwritten sting
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i;
+
+    for (i = 0; i < n ; i++)
+    {
+        *(dest + i) = *(src + i);
+    }
+
+    return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -35,5 +35,6 @@ char *leet(char *c);
 
 char *_memset(char *s, char b, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
