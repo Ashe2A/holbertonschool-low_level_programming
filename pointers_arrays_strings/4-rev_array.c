fc4d8fdd87b90c88d95742521e556d9ad23923f6
Ashe2A <supersonicx001@gmail.com>
2024-07-03 16:59:24 +0200

    sizeof

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -12,17 +12,17 @@ void reverse_array(int *a, int n)
 
 	if (n % 2 == 0)
 	{
-		half = n / 2;
+		half = (n * sizeof(int)) / 2;
 	}
 	else
 	{
-		half = (n - 1) / 2;
+		half = (n * sizeof(int)) / 2;
 	}
 
 	for (i = 0 ; (i < half) && (a[i] != '\0') ; i++)
 	{
 		tmp = a[i];
-		a[i] = a[(n - 1) - i];
-		a[(n - 1) - i] = tmp;
+		a[i] = a[((n * sizeof(int)) - 1) - i];
+		a[((n * sizeof(int)) - 1) - i] = tmp;
 	}
 }
