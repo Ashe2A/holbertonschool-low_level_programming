66f6a1e199893c129b243577be059cfe47d39f17
Ashe2A <supersonicx001@gmail.com>
2024-07-03 15:24:11 +0200

    5. to upper trial A

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - upper all lower
+ * @c: string to upper
+ * 
+ * Return: uppered string
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0 ; i < _strlen(c) ; i++)
+	{
+        if (c[i] >= 'a' && c[i] <= 'z')
+        {
+            c[i] += ('A' - 'a');
+        }
+    }
+}
