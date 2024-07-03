d90fb2f4fdca0d4b290d2febe19acd845933d8f5
Ashe2A <supersonicx001@gmail.com>
2024-07-04 00:52:58 +0200

    0 test 4

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -13,7 +13,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
     unsigned int i;
 
-    for (i = 0; (i < n) && s[i] != '\0' ; i++)
+    for (i = 0; s[i] != '\0' ; i++)
     {
         s[i] = b;
     }
