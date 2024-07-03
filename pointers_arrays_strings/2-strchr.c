7ba5cd0118d974d3fca3037c98432af0899f9cdc
Ashe2A <supersonicx001@gmail.com>
2024-07-04 01:37:47 +0200

    2 find char A

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strchr - finds a char in a string
+ * @s: string to look in
+ * @c: char to look for
+ *
+ * Return: pointer to the first occurence of c in s
+ */
+char *_strchr(char *s, char c)
+{
+    int i;
+
+    for (i = 0; s[i] != c; i++)
+    {
+    }
+
+    return (*(s + i));
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -36,5 +36,6 @@ char *leet(char *c);
 char *_memset(char *s, char b, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
