c832c34c1cf8efd6e9cfce7665ebf9719c5dab5c
Ashe2A <supersonicx001@gmail.com>
2024-07-05 16:19:35 +0200

    8D

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -16,7 +16,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0 ; i < size; i++)
 	{
 		diag1 += *(a + i);
-		diag2 += *(a + (size - 1) - i);
+		diag2 += *(a - i);
 	}
 
 	printf("%d, ", diag1);
