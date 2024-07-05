a341349cd0e1b7660bad9e3e4244910c671904a7
Ashe2A <supersonicx001@gmail.com>
2024-07-05 21:40:36 +0200

    4 add + use of local headers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "../pointers_arrays_strings/100-atoi.c"
+#include "../pointers_arrays_strings/2-strlen.c"
+
+/**
+ * main - multiply arguments
+ * @argc: Arguments count
+ * @argv: Arguments array
+ *
+ * Return: 0 (Success), 1 (Error if more than 2 args)
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, add = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < argc; i++)
+		{
+			for (j = 0; j < _strlen(argv[i]); j++)
+			{
+				if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			add += _atoi(argv[i]);
+		}
+
+		printf("%d\n", add);
+		return (0);
+	}
+}
