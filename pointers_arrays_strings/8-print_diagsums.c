5f943d6723b5cb1762ae2638e232698e8b91c995
Ashe2A <supersonicx001@gmail.com>
2024-07-05 16:02:22 +0200

    8 add diagonals

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * print_diagsums - adds up matrix diagonals
+ * @a: matrix
+ * @size: size of the matrix	
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int diag1 = 0;
+	int diag2 = 0;
+
+	for (i = 0 ; i < size; i++)
+	{
+		diag1 += a[i][i];
+		diag2 += a[0][(size - 1) - i];
+	}
+
+	printf("%d, ", diag1);
+	printf("%d\n", diag2);
+}
