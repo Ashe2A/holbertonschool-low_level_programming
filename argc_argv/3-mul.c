b1adb0744aeef4a1c0ae4c177bf1b5e408d51715
Ashe2A <supersonicx001@gmail.com>
2024-07-05 21:22:37 +0200

    use of atoi

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - multiply arguments
@@ -15,6 +16,6 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	printf("%s\n", argv[1] * argv[2]);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
