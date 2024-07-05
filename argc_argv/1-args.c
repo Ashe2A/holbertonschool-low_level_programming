17912660d0508c45ecb1749358e6a001547744df
Ashe2A <supersonicx001@gmail.com>
2024-07-05 20:54:53 +0200

    0 argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - number of arguments
+ * @argc: Arguments count
+ * @argv: Arguments array
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
