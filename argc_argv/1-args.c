47cef5ca1da101b49a71854c66682e44ad608afa
Ashe2A <supersonicx001@gmail.com>
2024-07-05 21:10:06 +0200

    2 all args

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - number of arguments
+ * main - all arguments
  * @argc: Arguments count
  * @argv: Arguments array
  *
  * Return: 0 (Success)
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
 	return (0);
 }
