3f2c2989467544ebab8101f86a906c2aaead045e
Ashe2A <supersonicx001@gmail.com>
2024-07-05 18:04:35 +0200

    1 recursive reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string
+ * @s: string to print
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
---
+++ recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef WEEK5_PROJECT1
+#define WEEK5_PROJECT1
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+
+#endif
