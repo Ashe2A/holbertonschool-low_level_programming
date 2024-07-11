de8a685c4add6f6f1e40d74d863c79e6a945dffc
Ashe2A <supersonicx001@gmail.com>
2024-07-11 05:41:45 +0200

    3 matrix alloc

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - generate matrix
+ * @width: matrix width
+ * @height: matrix height
+ *
+ * Return: resulting matrix
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **matrix;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	matrix = malloc(sizeof(int) * width * height);
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			matrix[i][j] = 0;
+		}
+	}
+
+	matrix[i][j] = '\0';
+
+	return (matrix);
+}
