5605f81841259f33c860d65b417e2b36a7790f50
Ashe2A <supersonicx001@gmail.com>
2024-07-12 14:07:11 +0200

    free all

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j;
+	int i, j, k;
 	int **matrix;
 
 	if (width <= 0 || height <= 0)
@@ -33,6 +33,11 @@ int **alloc_grid(int width, int height)
 
 		if (matrix[i] == NULL)
 		{
+			for (k = 0; k <= i; k++)
+			{
+				free(matrix[i]);
+			}
+
 			free(matrix);
 			return (NULL);
 		}
