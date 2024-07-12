76caa6101e47101af13735edef55758c799dd06e
Ashe2A <supersonicx001@gmail.com>
2024-07-12 16:51:52 +0200

    concatenes two strings

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenes two strings
+ * @b: size the allocated memory
+ *
+ * Return: filled array
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int head_s2, i, j;
+	char *strcat;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	if (n < _strlen(s2))
+	{
+		head_s2 = n;
+	}
+	else
+	{
+		head_s2 = _strlen(s2);
+	}
+
+	strcat = (char *)malloc(sizeof(char) * (_strlen(s1) + head_s2) + 1);
+
+	if (strcat == NULL || strcat == 0)
+	{
+		return (strcat);
+	}
+
+	for (i = 0; i < _strlen(s1); i++)
+	{
+		strcat[i] = s1[i];
+	}
+
+	for (j = 0; j < head_s2; j++)
+	{
+		strcat[j + i] = s2[j];
+	}
+
+	strcat[j + i] = '\0';
+
+	return (strcat);
+}
+
+/**
+ * _strlen - string length
+ * @s: string (pointer of char)
+ *
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		/* Counter */
+	}
+
+	return (i);
+}
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,6 +1,14 @@
-#ifndef WEEK6_PROJECT0
-#define WEEK6_PROJECT0
+#ifndef WEEK6_PROJECT1
+#define WEEK6_PROJECT1
 
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+#endif
+
+#ifndef MISCELLANEOUS
+#define MISCELLANEOUS
+
+int _strlen(char *s);
 
 #endif
