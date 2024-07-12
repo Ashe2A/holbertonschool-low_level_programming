6895945c6038c244c057dfecd288762a95c80055
Ashe2A <supersonicx001@gmail.com>
2024-07-12 23:49:06 +0200

    betty doc

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOGGIE
 #define DOGGIE
 
+/**
+ * struct dog - Dogs
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Dog's owner
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
