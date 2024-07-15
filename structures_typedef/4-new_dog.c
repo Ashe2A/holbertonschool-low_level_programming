341b6f2a223496211cec10fa041e9051a39e91f3
Ashe2A <supersonicx001@gmail.com>
2024-07-15 16:31:51 +0200

    4-new_dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * new_dog - makes a new dog
+ * @name: doggie name
+ * @age: doggie age
+ * @owner: dog owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t doggie;
+
+	init_dog(doggie, name, age, owner);
+
+	if (doggie == NULL)
+	{
+		return (NULL);
+	}
+
+	return (doggie);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,7 +14,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t ;
+} dog_t;
 
 #endif
 
@@ -23,5 +23,6 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
