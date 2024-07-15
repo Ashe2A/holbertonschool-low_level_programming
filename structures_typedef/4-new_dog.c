bd50e2fd9d00394a77be02f098d20f4376f7aba3
Ashe2A <supersonicx001@gmail.com>
2024-07-15 17:09:47 +0200

    4 name and owner copies F

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "dog.h"
 #include "1-init_dog.c"
+#include "../pointers_arrays_strings/9-strcpy.c"
 
 /**
  * new_dog - makes a new dog
@@ -12,17 +13,25 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggie;
+	char *namecpy, ownercpy;
 
 	doggie = malloc(sizeof(name) + sizeof(age) + sizeof(owner));
 
 	init_dog(doggie, name, age, owner);
 
-	if (doggie == NULL)	
+	if (doggie == NULL)
 	{
 		return (NULL);
 	}
 
+	namecpy = malloc(sizeof(name));
+	ownercpy = malloc(sizeof(owner));
+	_strcpy(namecpy, name);
+	_strcpy(ownercpy, owner);
+
 	free(doggie);
+	free(namecpy);
+	free(ownercpy);
 
 	return (doggie);
 }
