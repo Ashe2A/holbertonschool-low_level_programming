4002207933a9b61ed6691d213a5c623083dccb9c
Ashe2A <supersonicx001@gmail.com>
2024-07-16 10:19:15 +0200

    0 NULL pointer

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
@@ -7,5 +8,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+	if (name != NULL || f != NULL)
+	{
+		f(name);
+	}
 }
