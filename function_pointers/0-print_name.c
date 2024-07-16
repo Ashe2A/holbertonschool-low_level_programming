79714e2b4746288d660db67a95e983ccdfdc8313
Ashe2A <supersonicx001@gmail.com>
2024-07-16 14:28:10 +0200

    0 return

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -7,8 +7,12 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && f != NULL)
-    {
-        f(name);
-    }
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+	else
+	{
+		return;
+	}
 }
