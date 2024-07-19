11ed37808dab9efbfd4516868abd64cf9b5fc95b
Ashe2A <supersonicx001@gmail.com>
2024-07-19 17:02:30 +0200

    3 no for and only one while

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -17,14 +17,16 @@ int (*get_op_func(char *s))(int, int)
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}};
-	int i;
+	int i = 0;
 
-	for (i = 0; i < (int)sizeof(ops); i++)
+	while (i < (int)sizeof(ops))
 	{
 		if (strcmp(s, ops[i].op) == 0)
 		{
 			return (ops[i].f);
 		}
+
+		i++;
 	}
 
 	return (0);
