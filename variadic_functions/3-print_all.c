df5bcab2cd906875ecdbf45777b462702ddae5d3
Ashe2A <supersonicx001@gmail.com>
2024-07-20 00:56:14 +0200

    3 missing parenthesis

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -21,7 +21,7 @@ void print_all(const char *const format, ...)
 	va_list params;
 
 	va_start(params, format);
-	while (format[i] != '\0' || format != NULL)
+	while ((format[i] != '\0') || (format != NULL))
 	{
 		j = 0;
 
