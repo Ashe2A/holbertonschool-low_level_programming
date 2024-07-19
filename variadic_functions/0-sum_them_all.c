06ae4ced0a959bc4979d2001e4e4c9685dadacce
Ashe2A <supersonicx001@gmail.com>
2024-07-19 20:30:08 +0200

    0 variadic sum

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * sum_them_all - sum several integers
+ * @n: integers to sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, res;
+	va_list terms;
+
+	return (n == 0);
+
+	va_start(terms, n);
+	
+	terms = n;
+	while (va_arg(terms, int) != NULL)
+	{
+		res += va_arg(terms, int);
+	}
+
+	va_end(terms);
+
+	return (res);
+}
