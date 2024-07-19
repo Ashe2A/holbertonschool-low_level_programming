5529ad536654c3f472e6f4be7c567ba7a07ed2ab
Ashe2A <supersonicx001@gmail.com>
2024-07-19 17:04:02 +0200

    3 error new lines

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -16,20 +16,20 @@ int main(int argc, char *argv[])
 
 	if (argc != 4)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(98);
 	}
 
 	if (get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])) == 0)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(99);
 	}
 
 	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
 	&& atoi(argv[3]) == 0)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(100);
 	}
 
