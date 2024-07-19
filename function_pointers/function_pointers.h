6afced694bd3766453712084eca8978627c63f22
Ashe2A <supersonicx001@gmail.com>
2024-07-19 15:30:35 +0200

    0 betty final

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
-#include <stddef.h>
-
 #ifndef WEEK7_PROJECT0
 #define WEEK7_PROJECT0
+
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
