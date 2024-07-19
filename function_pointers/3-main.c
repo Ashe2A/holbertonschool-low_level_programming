049b9f86340b2d60f48307b6d0d893590d68faa9
Ashe2A <supersonicx001@gmail.com>
2024-07-19 19:20:15 +0200

    3 error order

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -20,17 +20,17 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])) == 0)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
 	if (((strcmp(argv[2], "/") == 0) || (strcmp(argv[2], "%") == 0))
 	&& (atoi(argv[3]) == 0))
 	{
 		printf("Error\n");
 		exit(100);
+	}
+
+	if (get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])) == 0)
+	{
+		printf("Error\n");
+		exit(99);
 	}
 
 	op_res = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
