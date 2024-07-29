f0426cbc7d8584704ba9e1a73921a0489165b7f5
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-07-29 16:10:28 +0200

    2 - free retry

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -16,7 +16,18 @@ list_t *add_node(list_t **head, const char *str)
 	if ((head != NULL) && (str != NULL))
 	{
 		new_head = malloc(sizeof(list_t));
+		if (new_head == NULL)
+		{
+			free(new_head);
+			return (NULL);
+		}
 		new_head->str = strdup(str);
+		if (new_head->str == NULL)
+		{
+			free(new_head->str);
+			free(new_head);
+			return (NULL);
+		}
 		new_head->len = strlen(str);
 		new_head->next = *head;
 		*head = new_head;
