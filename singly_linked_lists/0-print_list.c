d7f23f84e4a003bd7ac69733504873a5a65a12e2
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-07-29 14:40:36 +0200

    0 - arrows instead of dots + betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,17 +9,14 @@
  * Return: size of the list
  */
 size_t print_list(const list_t *h)
-{
-    list_t *new;
-    size_t size = 0;
-
-    if (h.str == NULL)
-    {
-        printf("[0] (nil)\n");
-        return (0);
-    }
-    printf("[%d] %s\n", h.len, h.str);
-    if (h.next == NULL)
-        return (h.len);
-    return (h.len + print_list(h.next));
+{	
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+		return (0);
+	}
+	printf("[%d] %s\n", h->len, h->str);
+	if (h->next == NULL)
+		return (h->len);
+	return (h->len + print_list(h->next));
 }
