22abd9c9118682b0bd8d371876577a2d1347e8b0
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-07-29 15:23:56 +0200

    2 - add node in list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * list_len - add node in list
+ * @head: address of the list head
+ * @str: str of the list head
+ *
+ * Return: newly added node address
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	if ((head != NULL) && (str != NULL))
+	{
+        strdup(head->next->str, str);
+        head->next->len = head->len;
+		return (head->next);
+	}
+	return (NULL);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -19,6 +19,7 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *);
-size_t list_len(const list_t *h);
+size_t list_len(const list_t *);
+list_t *add_node(list_t **, const char *);
 
 #endif
