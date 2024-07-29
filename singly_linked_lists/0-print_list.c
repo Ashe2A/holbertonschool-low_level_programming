65e8c706fe381a4cfe088e240cfaaf3e257ef350
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-07-29 14:47:10 +0200

    0 - still return before print when NULL

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,6 +13,7 @@ size_t print_list(const list_t *h)
 	if (h->str == NULL)
 	{
 		printf("[0] (nil)\n");
+		return (print_list(h->next) + 1);
 	}
 	printf("[%d] %s\n", h->len, h->str);
 	if (h->next == NULL)
