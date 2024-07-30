73c908898b646a6de7faf89a0bf3e7a21f7c29c4
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-07-30 15:05:34 +0200

    3 - apply 2 edits to 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,15 +13,23 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_tail = malloc(sizeof(list_t));
 	list_t *tail = head[0];
-	if ((head != NULL) && (str != NULL) && (tail != NULL) && (new_tail != NULL))
+
+	if ((head != NULL) && (str != NULL) && (new_tail != NULL))
 	{
-		new_tail->str = strdup(str);
-		new_tail->len = strlen(str);
 		while (tail->next != NULL)
 			tail = tail->next;
 		tail->next = new_tail;
+		new_tail->str = strdup(str);
+		if (new_tail->str == NULL)
+		{
+			free(new_tail->str);
+			free(new_tail);
+			return (NULL);
+		}
+		new_tail->len = strlen(new_tail->str);
 		return (new_tail);
 	}
+	free(new_tail);
 	return (NULL);
 }
 
