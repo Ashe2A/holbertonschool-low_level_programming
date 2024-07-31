8ad9d765fbba457054636d0812a03ef0e3832ded
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-01 00:21:24 +0200

    3 - new tail to a doubly list

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint_end - add new tail node in list
+ * @head: address of the list head
+ * @n: head first int
+ *
+ * Return: newly added node address
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_tail = malloc(sizeof(dlistint_t));
+	dlistint_t *no_dupes = head[0];
+	dlistint_t *old_tail;
+
+	if (new_tail != NULL)
+	{
+		while (no_dupes != NULL)
+		{
+			if (no_dupes->n == n)
+				return (NULL);
+			old_tail = no_dupes;
+			no_dupes = no_dupes->next;
+		}
+		new_tail->n = n;
+		new_tail->next = NULL;
+		if (old_tail == NULL)
+			new_tail->prev = NULL;
+		else
+		{
+			new_tail->prev = old_tail;
+			old_tail->next = new_tail;
+		}
+		old_tail = new_tail;
+		return (new_tail);
+	}
+	free(new_tail);
+	return (NULL);
+}
