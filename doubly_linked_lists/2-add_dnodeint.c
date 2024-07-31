e0bdbb8f79fa4bd8a293655a02a04ad5373f937a
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-07-31 16:52:52 +0200

    2 - head can be NULL

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,17 +12,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_head = malloc(sizeof(dlistint_t));
 
-	if ((head != NULL) && (new_head != NULL))
+	if (new_head != NULL)
 	{
 		new_head->n = n;
-		new_head->next = head[0];
-		if (new_head == NULL)
+		new_head->prev = NULL;
+		if (head != NULL)
 		{
-			free(new_head);
-			return (new_head);
+			new_head->next = head[0];
 		}
 		head[0] = new_head;
-		new_head->prev = NULL;
 		return (new_head);
 	}
 	free(new_head);
