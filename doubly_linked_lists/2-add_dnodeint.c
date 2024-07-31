e18d8fb69b66690a5dcde2b0fb750c61cad48eac
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-07-31 16:29:39 +0200

    2 - working on doublies

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	list_t *new_head = malloc(sizeof(dlistint_t));
+	dlistint_t *new_head = malloc(sizeof(dlistint_t));
 
 	if ((head != NULL) && (n != NULL) && (new_head != NULL))
 	{
