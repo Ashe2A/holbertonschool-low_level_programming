7218f3676b015a2079d223bc3323b250ef17fb44
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-07-31 10:03:50 +0200

    2 - free only the structure

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -18,7 +18,6 @@ list_t *add_node(list_t **head, const char *str)
 		new_head->str = strdup(str);
 		if (new_head->str == NULL)
 		{
-			free(new_head->str);
 			free(new_head);
 			return (NULL);
 		}
