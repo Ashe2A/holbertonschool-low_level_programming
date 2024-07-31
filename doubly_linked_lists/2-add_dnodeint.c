0fdd31a5d401dd09c2d67934ead5bfae5a5e4a7f
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-07-31 20:36:35 +0200

    2 - head->prev + no dupes

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,13 +11,25 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_head = malloc(sizeof(dlistint_t));
+	dlistint_t *no_dupes = head[0];
 
+	while (no_dupes != NULL)
+	{
+		if (no_dupes->n == n)
+			return (NULL);
+		no_dupes = no_dupes->next;
+	}
 	if (new_head != NULL)
 	{
 		new_head->n = n;
 		new_head->prev = NULL;
-		if (head != NULL)
+		if (head == NULL)
+			new_head->next = NULL;
+		else
+		{
 			new_head->next = head[0];
+			(head[0])->prev = new_head;
+		}
 		head[0] = new_head;
 		return (new_head);
 	}
