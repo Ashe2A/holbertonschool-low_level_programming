2f990de09c7c23ac7b4ff9f5b89d7107ff8dcd51
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-01 17:22:53 +0200

    3 - no dupes can't be tested before head

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,6 +16,14 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if (new_tail != NULL)
 	{
+		new_tail->n = n;
+		new_tail->next = NULL;
+		if(head[0] == NULL)
+		{
+			new_tail->prev = NULL;
+			head[0] = new_tail;
+			return (new_tail);
+		}
 		while (no_dupes != NULL)
 		{
 			if (no_dupes->n == n)
@@ -25,14 +33,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 			}
 			old_tail = no_dupes;
 			no_dupes = no_dupes->next;
-		}
-		new_tail->n = n;
-		new_tail->next = NULL;
-		if(head[0] == NULL)
-		{
-			new_tail->prev = NULL;
-			head[0] = new_tail;
-			return (new_tail);
 		}
 		new_tail->prev = old_tail;
 		old_tail->next = new_tail;
