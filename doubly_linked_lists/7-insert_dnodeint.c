05087bf0e0931e33d55c91b4f75d35103b0ed8af
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-08-19 15:01:05 +0200

    7 - head of h

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	next_node = get_dnodeint_at_index(h, idx);
+	next_node = get_dnodeint_at_index(h[0], idx);
 
 	if (new_node != NULL)
 	{
