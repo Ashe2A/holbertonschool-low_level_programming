0ededa06b48656ee9b875a31194a9ea41c5513dc
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-08-19 13:41:48 +0200

    4 - free doubly list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - free a list
+ * @head: address of the list head
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head != NULL)
+	{
+		do
+		{
+			head = head->next;
+			free(head->prev);
+		} while (head->next != NULL);
+		free(head);
+	}
+}
