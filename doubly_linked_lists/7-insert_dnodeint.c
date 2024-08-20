27ed4576f2376cd596e12d24e318621f4f2f4bce
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-08-21 00:19:01 +0200

    7 - back to length comparison

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,9 +15,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-	if ((new_node != NULL) && (h != NULL)
-	&& (get_dnodeint_at_index(h[0], idx) != NULL))
+	if ((new_node != NULL) && (h != NULL))
 	{
+		if (idx > dlistint_len(h[0]) - 1)
+			free(new_node);
+			return (NULL);
 		new_node->n = n;
 		new_node->prev = get_dnodeint_at_index(h[0], idx - 1);
 		new_node->next = get_dnodeint_at_index(h[0], idx);
