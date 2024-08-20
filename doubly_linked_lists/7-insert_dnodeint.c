5599e6ecd72de9bed32839a61a3337ce1e514a85
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-08-20 23:47:46 +0200

    7 - NULL edges

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -23,9 +23,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (new_node->prev != NULL)
 			new_node->prev->next = new_node;
 		else
+		{
 			h[0] = new_node;
+			new_node->prev = NULL;
+		}
 		if (next_node != NULL)
 			next_node->prev = new_node;
+		else
+			new_node->next = NULL;
 		return (new_node);
 	}
 	free(new_node);
