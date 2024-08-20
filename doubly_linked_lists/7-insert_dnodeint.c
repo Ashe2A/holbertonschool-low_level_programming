7ff9e143a826f935e97e256bcfb887ce6b36eaaf
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-08-21 00:10:14 +0200

    7 - simpler

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,24 +13,18 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	dlistint_t *next_node = get_dnodeint_at_index(h[0], idx);
 
-	if ((new_node != NULL) && (next_node != NULL))
+	if ((new_node != NULL) && (h != NULL))
 	{
 		new_node->n = n;
-		new_node->prev = next_node->prev;
-		new_node->next = next_node;
+		new_node->prev = get_dnodeint_at_index(h[0], idx - 1);
+		new_node->next = get_dnodeint_at_index(h[0], idx);
 		if (new_node->prev != NULL)
 			new_node->prev->next = new_node;
 		else
-		{
 			h[0] = new_node;
-			new_node->prev = NULL;
-		}
-		if (next_node != NULL)
-			next_node->prev = new_node;
-		else
-			new_node->next = NULL;
+		if (new_node->next != NULL)
+			new_node->next->prev = new_node;
 		return (new_node);
 	}
 	free(new_node);
