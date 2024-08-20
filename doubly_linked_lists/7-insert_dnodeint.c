fce86eba072d8fde73313d5a7b0c058ed7cefd22
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-08-21 01:42:25 +0200

    idx can't be negative

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -18,9 +18,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if ((new_node != NULL) && (h != NULL))
 	{
 		new_node->n = n;
-		new_node->prev = get_dnodeint_at_index(h[0], idx - 1);
+		if (idx >= 1)
+			new_node->prev = get_dnodeint_at_index(h[0], idx - 1);
+		else
+			new_node->prev = NULL;
 		new_node->next = get_dnodeint_at_index(h[0], idx);
-		if ((new_node->prev != NULL) && (idx != 0))
+		if (new_node->prev != NULL)
 			new_node->prev->next = new_node;
 		else
 			h[0] = new_node;
