6e3a7a06315079c7b61877e9019620a3f385805f
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-08-21 10:34:26 +0200

    0 - i needs to reach 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -30,7 +30,7 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b != NULL)
 	{
-		for (i = binlen - 1; i > 0; i++)
+		for (i = binlen - 1; i >= 0; i++)
 		{
 			if ((b[i] != '0') && (b[i] != '1'))
 				return (0);
