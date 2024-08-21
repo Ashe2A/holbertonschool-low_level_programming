d58b3886781dfd0a6c251d7bae764eed4fe673a3
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-08-21 03:17:28 +0200

    7 - stand corrected

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -22,12 +22,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (idx == dlistint_len(h[0]))
 			return (add_dnodeint_end(h, n));
 		new_node->n = n;
-		new_node->prev = get_dnodeint_at_index(h[0], idx - 1);
 		new_node->next = get_dnodeint_at_index(h[0], idx);
+		new_node->prev = get_dnodeint_at_index(h[0], idx - 1);
 		if (new_node->prev != NULL)
 			new_node->prev->next = new_node;
-		else
-			h[0] = new_node;
 		if (new_node->next != NULL)
 			new_node->next->prev = new_node;
 		return (new_node);
