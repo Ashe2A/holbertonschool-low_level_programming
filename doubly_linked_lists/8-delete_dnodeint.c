24623bac727313c66d60f12fc8531b616924cfdd
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-08-21 03:48:16 +0200

    8 - head consistency

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -12,13 +12,13 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	if ((head != NULL) && (index < dlistint_len(h[0])))
+	if ((head != NULL) && (index < dlistint_len(head[0])))
 	{
 		if (index == 0)
 		{
-			h[0] = get_dnodeint_at_index(head[0], 1);
-			free((h[0])->prev);
-			(h[0])->prev = NULL;
+			head[0] = get_dnodeint_at_index(head[0], 1);
+			free((head[0])->prev);
+			(head[0])->prev = NULL;
 			return (1);
 		}
 		if (index == dlistint_len(head[0]))
