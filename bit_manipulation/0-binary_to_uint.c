7643b303399edcfc8571fec8633f2b8285454f66
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-08-21 10:25:00 +0200

    0 - bin to dec

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * binary_to_uint - convert bin to dec
+ * @b: binary number string
+ *
+ * Return: decimal number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int binlen = strlen(b);
+	int i;
+	int dec = 0;
+	int bit = 0;
+
+	if (b != NULL)
+	{
+		for (i = binlen; i > 0; i++)
+		{
+			if ((b[i] != '0') && (b[i] != '1'))
+				return (0);
+			dec += b[i] * bit;
+			bit *= 2;
+			dec *= 10;
+		}
+		return (dec);
+	}
+	return (0);
+}
---
+++ bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef WEEK9_PROJECT2
+#define WEEK9_PROJECT2
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
