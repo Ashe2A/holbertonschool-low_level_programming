2f39ae1762819a6865165291260252d751f7f78d
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-08-22 13:53:54 +0200

    2 - rigourous

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * get_bit - get bit at index of a number
@@ -9,7 +10,16 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > 63) /* (number of bits of ULONG_MAX) - 1 */
+	unsigned long int ulm = ULONG_MAX;
+	unsigned int i = 0;
+ 
+	while (ulm > 0)
+	{
+		ulm >> 1;
+		i++;
+	}
+		
+	if (index > i) /* (number of bits of ULONG_MAX) - 1 */
 		return (-1);
 	return ((n >> index) & 1);
 }
