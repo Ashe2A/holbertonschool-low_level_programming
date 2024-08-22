4a38bd7bfec86c7b215345a8e48d40d89009a598
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-08-22 13:55:08 +0200

    2 - betty

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -12,13 +12,13 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int ulm = ULONG_MAX;
 	unsigned int i = 0;
- 
+
 	while (ulm > 0)
 	{
 		ulm >>= 1;
 		i++;
 	}
-		
+	
 	if (index > i) /* (number of bits of ULONG_MAX) - 1 */
 		return (-1);
 	return ((n >> index) & 1);
